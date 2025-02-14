73249357cbecbf3735fb8e0c62b735c1e6482ba4
jacobweiler <zed8drift@gmail.com>
2025-02-14 11:07:18 -0500

    Fixed Material Definition
    - Added FS-20 Ferrite Material Defintion which has gotten gains in line with what we would expect
    - Minor changes to gridding that were adjusted while debugging low-frequency gain patterns (doesn't seem to change much though)
    - Setup file for testing

--- Xmacros/CreateFerrite.js
+++ Xmacros/CreateFerrite.js
@@ -1,31 +1,37 @@
 // ferrite function defintion
 function CreateFerrite()
 {
-
+    // Adding in sampled data (based on stephs fs-20 material)
     var mSampled = new MagneticSampledParameters()
-
-    mSampled.addFrequency("1 GHz", 1,0)
+    mSampled.addFrequency("0.1 GHz",15, "413 ohm/m")
+    mSampled.addFrequency("0.2 GHz",18, "1990 ohm/m")
+    mSampled.addFrequency("0.3 GHz",16, "26530 ohm/m")
+    mSampled.addFrequency("0.4 GHz",11, "34741 ohm/m")
+    mSampled.addFrequency("0.5 GHz",10, "39500 ohm/m")
+    mSampled.addFrequency("0.6 GHz",10, "47400 ohm/m")
+    mSampled.addFrequency("0.7 GHz",10, "55300 ohm/m")
+    mSampled.addFrequency("0.8 GHz",10, "63200 ohm/m")
+    mSampled.addFrequency("0.9 GHz",10, "71100 ohm/m")
+    mSampled.addFrequency("1 GHz",10, "79000 ohm/m")
 
     var ferrite = new Material();
-
     ferrite.name = "ferrite";
-
     var ferriteElectricIsotropic = new ElectricIsotropic();
 
+    var electricNormalParams = new ElectricNormalParameters();
+    electricNormalParams.setConductivity("0 S/m");
+    electricNormalParams.setRelativePermittivity("10");
+    ferriteElectricIsotropic.setParameters(electricNormalParams);
+
     var ferriteMagneticIsotropic = new MagneticIsotropic(mSampled);
-
     var ferritePhysicalMaterial = new PhysicalMaterial();
-
     ferritePhysicalMaterial.setElectricProperties( ferriteElectricIsotropic );
-
     ferritePhysicalMaterial.setMagneticProperties( ferriteMagneticIsotropic );
 
     ferrite.setDetails( ferritePhysicalMaterial );
 
     var ferriteBodyAppearance = ferrite.getAppearance();
-
     var ferriteFaceAppearance = ferriteBodyAppearance.getFaceAppearance();
-
     ferriteFaceAppearance.setColor( new Color( 200, 100, 200, 200) );
 
     if(null != App.getActiveProject().getMaterialList().getMaterial(ferrite.name) )
--- Xmacros/CreateGrid.js
+++ Xmacros/CreateGrid.js
@@ -4,11 +4,11 @@ function CreateGrid()
     // Set up the grid spacing for the antenna
     var grid = App.getActiveProject().getGrid();
     var cellSizes = grid.getCellSizesSpecification();
-
-    cellSizes.setTargetSizes( Cartesian3D( 3 + units, 3 + units, 3 + units ) );
+    useProGridCellSizes = false;
+    cellSizes.setTargetSizes( Cartesian3D( 0.4 + units, 0.4 + units, 0.4 + units ) );
     // And we need to set the Minimum Sizes - these are the minimum deltas that we will allow in this project.
     // We'll use the scalar ratio of 20% here.
-    cellSizes.setMinimumSizes( Cartesian3D( ".5", ".5", ".5" ) );
+    cellSizes.setMinimumSizes( Cartesian3D( ".05", ".05", ".05" ) );
     cellSizes.setMinimumIsRatioX( true );
     cellSizes.setMinimumIsRatioY( true );
     cellSizes.setMinimumIsRatioZ( true );
