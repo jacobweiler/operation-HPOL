f5c66c5a9a8f40eae7858fa81ae81e40438eb148
jacobweiler <zed8drift@gmail.com>
2025-02-18 11:27:20 -0500

    Fixed HPOL Ara job submitting bug
    - somehow a space got added between imported variables for the sbatch command...

--- Loop_Parts/Part_D/Part_D1_HPOL.sh
+++ Loop_Parts/Part_D/Part_D1_HPOL.sh
@@ -30,7 +30,7 @@ nnt_per_ara=$((NNT / (Seeds * threads_per_ara_job)))
 
 mkdir -m777 $RunDir/AraSim_Outputs/${gen}_AraSim_Outputs
 
-sbatch 	--array=1-${num_jobs}%${maxJobs} --export=ALL,gen=$gen,WorkingDir=$WorkingDir,RunName=$RunName,Seeds=$Seeds,AraSimDir=$AraSimExec, nnt_per_ara=$nnt_per_ara \
+sbatch 	--array=1-${num_jobs}%${maxJobs} --export=ALL,gen=$gen,WorkingDir=$WorkingDir,RunName=$RunName,Seeds=$Seeds,AraSimDir=$AraSimExec,nnt_per_ara=$nnt_per_ara \
 		--job-name=${RunName} --output=$output_name --error=$error_name \
 		Batch_Jobs/${job_name}
 		
